e1353aea80425503c7562b9133cd76faf9710ce4
nalla-msv <msvenkatesh0709@gmail.com>
2025-05-22 16:01:54 +0200

    update typewriter

--- js/functions.js
+++ js/functions.js
@@ -71,26 +71,29 @@ function startHeartAnimation() {
 }
 
 (function($) {
-	$.fn.typewriter = function() {
-		this.each(function() {
-			var $ele = $(this), str = $ele.html(), progress = 0;
-			$ele.html('');
-			var timer = setInterval(function() {
-				var current = str.substr(progress, 1);
-				if (current == '<') {
-					progress = str.indexOf('>', progress) + 1;
-				} else {
-					progress++;
-				}
-				$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
-				if (progress >= str.length) {
-					clearInterval(timer);
-				}
-			}, 75);
-		});
-		return this;
-	};
+    $.fn.typewriter = function() {
+        this.each(function() {
+            var $ele = $(this), str = $ele.html(), progress = 0;
+            $ele.html('');
+            var timer = setInterval(function() {
+                var current = str.substr(progress, 1);
+                if (current == '<') {
+                    progress = str.indexOf('>', progress) + 1;
+                } else {
+                    progress++;
+                }
+                if (progress < str.length) {
+                    $ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
+                } else {
+                    $ele.html(str);
+                    clearInterval(timer);
+                }
+            }, 75);
+        });
+        return this;
+    };
 })(jQuery);
+
 
 function timeElapse(date){
 	var current = Date();
